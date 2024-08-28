f71f210af200929c4a8cc46dbc6340db56d1b79f
Anshif NIJU <anshifniju369@gmail.com>
2024-08-28 13:03:25 +0530

    Java Scipt Completed'

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-function click(a)
+function buttonclick(val)
 {
-out=a
-document.getElementById('out').innerHTML()
+    
+    document.getElementById('screen').value+=val
 
 
 
+
+}
+function clearDisplay()
+{
+document.getElementById('screen').value=""
+
+
+}
+function equalclick()
+{
+
+var text=document.getElementById('screen').value
+ result=eval(text)
+ document.getElementById('screen').value=result
 
 
 
